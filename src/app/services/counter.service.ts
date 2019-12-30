863c1a8d247f96b418ecb14ce7d7735170ca5a94
erwanlbp <erwan.le-batard-poles@setec.com>
2019-12-30 23:01:36 +0100

    fetch counters from Firebase

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,30 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Counter } from '../interfaces/counter';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CounterService {
 
-    constructor() {
+    constructor(
+        private firestore: AngularFirestore,
+        private fireauth: AngularFireAuth,
+    ) {
     }
 
     fetchCounters(): Observable<Counter[]> {
-        return of(
-            [
-                {name: 'Patates', value: 5},
-                {name: 'Patates', value: 5},
-                {name: 'Patates', value: 5},
-                {name: 'Patates', value: 5},
-                {name: 'Patates', value: 5},
-                {name: 'Patates', value: 5},
-                {name: 'Patates', value: 5},
-                {name: 'Patates', value: 5},
-                {name: 'Patates', value: 5},
-                {name: 'Patates', value: 5},
-                null,
-            ]
+        return this.fireauth.authState.pipe(
+            switchMap(user => {
+                if (!user) {
+                    return of([]);
+                }
+                console.log(user);
+                return this.firestore.collection<Counter[]>(`users/${user.uid}/counters`).valueChanges();
+            }),
+            tap(x => console.log(x)),
         );
     }
 }
