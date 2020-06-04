0ecf4ca6e3a7d1f9059a5bcb39dcd8e5426ef373
erwanlbp <erwan.le-batard-poles@setec.com>
2020-06-04 23:37:17 +0200

    add functions/other/* to manipulate firestore data (creating monthly aggregation)

---
+++ functions/other/test.ts
@@ -0,0 +1,12 @@
+import * as moment from 'moment';
+
+function getMonthDate(timestamp: number): number {
+
+    const x =  moment(timestamp).utc().startOf('month').startOf('day').valueOf();
+    console.log(x);
+    return x;
+
+}
+
+
+export default getMonthDate(1591025160000);
