6ae6bc13312b7dad0cd49131f10f38b9a3f6042c
erwanlbp <erwan.le-batard-poles@setec.com>
2020-06-08 12:06:47 +0200

    delete counter event monthly aggregate on delete counter

--- functions/src/delete-counter-events.ts
+++ functions/src/delete-counter-events.ts
@@ -1,12 +1,23 @@
 import * as functions from 'firebase-functions';
-import * as admin from "firebase-admin";
+import * as admin from 'firebase-admin';
 
 export const func = functions
-    .region("europe-west1")
+    .region('europe-west1')
     .firestore.document('users/{userId}/counters/{counterId}')
     .onDelete((snapshot, context) => {
-        const {userId, counterId} = context.params;
-        return admin.firestore().collection(`users/${userId}/counters/${counterId}/events`).listDocuments()
-            .then(docs => docs.forEach(doc => doc.delete()))
-            .catch(err => console.log(err));
+        const { userId, counterId } = context.params;
+
+        const deleteCounterEvents = admin
+            .firestore()
+            .collection(`users/${userId}/counters/${counterId}/events`)
+            .listDocuments()
+            .then(docs => Promise.all(docs.map(async doc => await doc.delete())));
+
+        const deleteCounterMonthlyEvents = admin
+            .firestore()
+            .collection(`users/${userId}/counters/${counterId}/agg-monthly-events`)
+            .listDocuments()
+            .then(docs => Promise.all(docs.map(async doc => await doc.delete())));
+
+        return Promise.all([deleteCounterEvents, deleteCounterMonthlyEvents]).catch(err => console.log(err));
     });
