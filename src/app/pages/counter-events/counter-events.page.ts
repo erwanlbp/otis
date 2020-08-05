c0206fb8f31e019520211c239b5a1786e4ed68c1
erwanlbp <erwan.le-batard-poles@setec.com>
2020-08-05 17:02:24 +0200

    hide load more button if chunk fetched < requested chunk size

--- src/app/pages/counter-events/counter-events.page.ts
+++ src/app/pages/counter-events/counter-events.page.ts
@@ -10,29 +10,42 @@ import { switchMap } from 'rxjs/operators';
     styleUrls: ['./counter-events.page.scss'],
 })
 export class CounterEventsPage implements OnInit {
+
+    private static EVENTS_CHUNK_SIZE = 20;
+
     counterEvents: CounterEvent[];
     counterName: string;
     noMoreData: boolean = false;
 
-    constructor(private eventService: EventService, private activatedRoute: ActivatedRoute) {}
+    constructor(
+        private eventService: EventService,
+        private activatedRoute: ActivatedRoute,
+    ) {
+    }
 
     ngOnInit(): void {
         this.activatedRoute.params
             .pipe(
                 switchMap(params => {
                     this.counterName = params.counterName;
-                    return this.eventService.fetchChunkCounterEvents$(this.counterName, 20);
+                    return this.eventService.fetchChunkCounterEvents$(this.counterName, CounterEventsPage.EVENTS_CHUNK_SIZE);
                 }),
             )
-            .subscribe(events => (this.counterEvents = events));
+            .subscribe(events => {
+                this.counterEvents = events;
+                if (!events.length || events.length < CounterEventsPage.EVENTS_CHUNK_SIZE) {
+                    this.noMoreData = true;
+                }
+            });
     }
 
     loadMore() {
-        this.eventService.fetchChunkCounterEvents$(this.counterName, 20, this.counterEvents[this.counterEvents.length - 1].timestamp).subscribe(events => {
-            if (!events.length) {
-                this.noMoreData = true;
-            }
-            this.counterEvents.push(...events);
-        });
+        this.eventService.fetchChunkCounterEvents$(this.counterName, CounterEventsPage.EVENTS_CHUNK_SIZE, this.counterEvents[this.counterEvents.length - 1].timestamp)
+            .subscribe(events => {
+                if (!events.length || events.length < CounterEventsPage.EVENTS_CHUNK_SIZE) {
+                    this.noMoreData = true;
+                }
+                this.counterEvents.push(...events);
+            });
     }
 }
