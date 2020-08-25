3cd711db592905c1111995a5a65d661f11a7b73b
erwanlbp <erwan.le-batard-poles@setec.com>
2020-08-25 21:08:33 +0200

    better time counter tile

--- src/app/pipes/time-elapsed.pipe.ts
+++ src/app/pipes/time-elapsed.pipe.ts
@@ -1,17 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { interval, Observable, of } from 'rxjs';
 import * as moment from 'moment';
-import { map, take } from 'rxjs/operators';
+import { map, startWith, take } from 'rxjs/operators';
 
 @Pipe({
     name: 'timeElapsed',
 })
 export class TimeElapsedPipe implements PipeTransform {
 
+    private static convert(value: number): string {
+        return moment.duration(new Date().getTime() - value).format();
+    }
+
     transform(value: number): Observable<string> {
         if (!value) {
             return of('--').pipe(take(1));
         }
-        return interval(5000).pipe(map(() => moment.duration(new Date().getTime() - value).format()));
+        return interval(1000)
+            .pipe(
+                map(() => TimeElapsedPipe.convert(value)),
+                startWith(TimeElapsedPipe.convert(value)),
+            );
     }
 }
