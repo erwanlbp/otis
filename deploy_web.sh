59eafd7405ac4ac32f65793aeb9b3a0e68f35d0c
erwanlbp <erwan.le-batard-poles@setec.com>
2021-02-14 13:35:42 +0100

    update to 1.13.0

--- deploy_web.sh
+++ deploy_web.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-deploy_mesage=$1
+##──── Question 0 ────────────────────────────────────────────────────────────────────────
+deploy_message="git commit $(git rev-parse --short HEAD)"
+read -p "
+Quel message de déploiement ?
+	(press <enter> for '${deploy_message}') : " q0
+if [[ "${q0}" != "" ]]
+then
+	deploy_message="${q0}"
+fi
 
-firebase deploy --only hosting --message ${deploy_mesage}
+firebase deploy --only hosting --message "${deploy_mesage}"
 
 exit 0
